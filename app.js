e8cb3392597d1468fe0f5aa4b99445685c52ac6f
Dylan <kite.d92@gmail.com>
2018-11-30 12:21:11 -0800

    started server routes

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(morgan('dev-'))
+app.use(bodyParser.json())
+
+// app.use('/users', require('./routes/users'))
+
+app.use((req, res, next) => {
+    next({status:404, message: "Couldn't find it, bro"})
+})
+
+app.use((err, req, res, next) => {
+    const status = 500 || err.status
+    req.status(status).send(err.message)
+})
+
+const listener = () => console.log(`Getting lit on port ${port}`)
+app.listen(port, listener)
