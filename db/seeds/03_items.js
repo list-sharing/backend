2063d053f694d6a1c1a4048b6d553cb0d827082b
Dylan <kite.d92@gmail.com>
2018-12-03 11:14:20 -0800

    wrote seeds

--- db/seeds/03_items.js
+++ db/seeds/03_items.js
@@ -1,13 +1,11 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('items').insert([
+    { id: 1, list_id: 1, source_url: 'https://itsalwayssunny.fandom.com/wiki/Ocular_patdown', synopsis:'visual threat assessment'},
+    { id: 2, list_id: 1, source_url: 'https://www.youtube.com/watch?v=rve6icdL4O0', synopsis: 'The bar just got slightly more unsafe.' },
+    { id: 3, list_id: 2, source_url: 'http://bitterr.com/howtoplay.html', synopsis:'rules for how to play night crawlers'},
+    { id: 4, list_id: 2, source_url: 'https://i.ytimg.com/vi/VsiCxwyVXxQ/hqdefault.jpg', synopsis:'chicken plus jeans'},
+    { id: 5, list_id: 3, source_url: 'https://www.youtube.com/watch?v=coVsuFLzBe8', synopsis:'cool dance routines'},
+    { id: 6, list_id: 3, source_url: 'https://www.sofeminine.co.uk/skin-care/how-to-disguise-a-crooked-nose-with-makeup-s1630255.html', synopsis: 'how to hide a broken nose' }
+  ]);
 };
