77c9f3f358e0dd2d4368c9386c042bf31bbbcd3e
Dylan <kite.d92@gmail.com>
2018-12-03 10:17:59 -0800

    added signup route

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,14 @@
-const userModel = require('models/users')
+const userModel = require('../models/users')
 
+function signup(res, res, next){
+    const {email, password} = req.body
+    if(!email || !password) return next({status:400, message: 'Incomplete signup'})
+    return userModel.signup(email, password)
+    .then(([data]) => {
+        if(!data) return next({status:500, message: 'Something went wrong'})
+        res.status(201).send({message: `Account created for ${data}`})
+    })
+    .catch(next)
+}
 
-
-module.exports = {}
+module.exports = {signup}
