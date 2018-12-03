59f40d220f7668f95e74b82c3ae90b913b6bf5dc
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2018-12-03 11:42:10 -0800

    models, controllers and routes for items

--- db/knex.js
+++ db/knex.js
@@ -4,19 +4,4 @@ const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('../knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
-
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('listin_dev', table => {
-      table.increments()
-      table.string('users').notNullable()
-      table.string('lists').notNullable()
-      table.string('items').notNullable()
-      table.timestamps(true, true)
-    })
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('listin_dev')
-  };
-
 module.exports = knex;
