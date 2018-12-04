1575b344953c663014d8b37fb74836a61cc2617a
Dylan <kite.d92@gmail.com>
2018-12-03 17:53:23 -0800

    polished authentication and authorization

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const authCtrl = require('../controllers/auth-controllers')
 
 router.post('/login', authCtrl.login)
+router.get('/token', authCtrl.authenticate, authCtrl.authStatus)
 
 module.exports = router
