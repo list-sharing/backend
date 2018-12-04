3c4b03a5dccefb99fde4d6fb717316bc992bc0a6
Dylan <kite.d92@gmail.com>
2018-12-04 13:30:34 -0800

    schema change

--- db/seeds/04_user_lists.js
+++ db/seeds/04_user_lists.js
@@ -1,9 +1,9 @@
 
 exports.seed = function(knex, Promise) {
   return knex('users_lists').insert([
-    {id: 1, user_id:1, list_id: 1},
-    { id: 2, user_id: 2, list_id: 2 },
-    { id: 3, user_id: 3, list_id: 3 }
+    {id: 1, user_id:1, list_id: 1, is_owner:true},
+    { id: 2, user_id: 2, list_id: 2, is_owner: true},
+    { id: 3, user_id: 3, list_id: 3, is_owner: true }
   ])
   .then(() => {
     return knex.raw("SELECT setval('users_lists_id_seq', (SELECT MAX(id) FROM users_lists));")
